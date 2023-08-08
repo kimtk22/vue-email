8487a4d7b7269b2f9b6ce9f570f502db56a82319
kimtk <vodjaos@gmail.com>
2023-08-08 17:26:15 +0900

    사이드바 진행중

---
+++ src/components/UserComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="UserComponent">
+    <img
+      @:click="openMenu = !openMenu"
+      data-tooltip-target="tooltip-no-arrow-user"
+      data-tooltip-placement="bottom"
+      src="https://via.placeholder.com/40"
+      class="rounded-full w-8 cursor-pointer"
+    />
+    <div
+      v-show="!openMenu"
+      id="tooltip-no-arrow-user"
+      role="tooltip"
+      class="inline-block absolute invisible text-xs z-10 py-1 px-2 font-medium text-white bg-gray-900 rounded-sm shadow-sm opacity-0 tooltip dark:bg-gray-600 delay-150"
+    >
+      <div>Google Account</div>
+      <div class="text-gray-300">Kim TK</div>
+      <div class="text-gray-300">vodjaos@gmail.com</div>
+    </div>
+    <div
+      v-show="openMenu"
+      class="bg-white absolute top-14 right-2 rounded-lg z-10 w-80"
+    >
+      <div class="w-full flex justify-center">
+        <img
+          src="https://via.placeholder.com/70"
+          class="rounded-full w-20 mt-4"
+        />
+      </div>
+      <div class="text-gray-700 w-full flex justify-center mt-2 text-lg">
+        Kim TK
+      </div>
+      <div
+        class="text-gray-700 w-full flex justify-center mt-2 text-sm pb-4 border-b"
+      >
+        vodjaos@gmail.com
+      </div>
+      <div class="flex justify-center my-5">
+        <button
+          class="bg-transparent text-xs hover:bg-gray-100 text-gray-600 font-semibold py-2 px-4 border border-gray-300 rounded"
+        >
+          Sign out fo Gmail
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      openMenu: false,
+    };
+  },
+};
+</script>
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="EmailView" class="bg-gray-50 h-screen">
+    <div class="flex items-center p-2">
+      <div class="flex justify-start items-center min-w-[250px]">
+        <IconComponent
+          iconString="menu"
+          iconColor="#636363"
+          :iconSize="19"
+          hoverColor="hover:bg-gray-200"
+          text="Main menu"
+          class="-ml-2 mr-2"
+        />
+        <img src="../../../public/img/GmailLogo.png" width="107" />
+      </div>
+      <div class="flex w-full justify-between">
+        <div
+          class="w-full bg-gray-200 flex items-center p-1 px-2 rounded-lg max-w-[700px] min-w-[250px]"
+        >
+          <IconComponent
+            iconString="magnify"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Search"
+          />
+          <input
+            type="text"
+            class="w-full h-10 bg-gray-200 border-none border-transparent focus:ring-0 outline-none placeholder-gray-900"
+            placeholder="Search"
+          />
+          <IconComponent
+            iconString="tune"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Show search options"
+          />
+        </div>
+        <div class="flex items-center justify-between w-32 ml-6">
+          <IconComponent
+            iconString="cog"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Settings"
+          />
+          <IconComponent
+            iconString="apps"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Google Apps"
+          />
+          <UserComponent />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex w-full">
+      <div id="SideMenu" class="w-[300px]">
+        <div
+          class="flex items-center justify-center bg-sky-200 w-36 h-8 mt-2 rounded-2xl ml-2 p-7 cursor-pointer"
+        >
+          <PencilOutlineIcon size="25" class="mr-3" />
+          <span class="text-sm">Compose</span>
+        </div>
+        <div class="my-5"></div>
+        <div
+          class="flex w-[250px] items-center justify-between bg-blue-100 px-6 py-1.5 rounded-r-full"
+        >
+          <div class="flex items-center">
+            <InboxIcon size="17" />
+            <div class="text-sm pl-4 font-semibold">Inbox</div>
+          </div>
+          <div class="text-xs pl-4 font-semibold">26</div>
+        </div>
+        <div class="flex w-[250px] items-center justify-between px-6 py-1.5">
+          <div class="flex items-center">
+            <StarOutlineIcon size="17" />
+            <div class="text-sm pl-4">Starred</div>
+          </div>
+        </div>
+        <div class="flex w-[250px] items-center justify-between px-6 py-1.5">
+          <div class="flex items-center">
+            <ClockOutlineIcon size="17" />
+            <div class="text-sm pl-4">Snoozed</div>
+          </div>
+        </div>
+        <div class="flex w-[250px] items-center justify-between px-6 py-1.5">
+          <div class="flex items-center">
+            <SendOutlineIcon size="17" />
+            <div class="text-sm pl-4">Sent</div>
+          </div>
+        </div>
+        <div class="flex w-[250px] items-center justify-between px-6 py-1.5">
+          <div class="flex items-center">
+            <FileOutlineIcon size="17" />
+            <div class="text-sm pl-4">Draft</div>
+          </div>
+        </div>
+      </div>
+      <!-- this is where the page changes -->
+      <router-view />
+      <!-- this is where the page changes -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from "@/components/IconComponent.vue";
+import UserComponent from "@/components/UserComponent.vue";
+import PencilOutlineIcon from "vue-material-design-icons/PencilOutline.vue";
+import InboxIcon from "vue-material-design-icons/Inbox.vue";
+import StarOutlineIcon from "vue-material-design-icons/StarOutline.vue";
+import ClockOutlineIcon from "vue-material-design-icons/ClockOutline.vue";
+import SendOutlineIcon from "vue-material-design-icons/SendOutline.vue";
+import FileOutlineIcon from "vue-material-design-icons/FileOutline.vue";
+</script>
